eef7f0bd8c60de6ce58e035f5d0d7490dfa4020c
vibin-eng <khareanushka2015@gmail.com>
2023-03-08 21:47:11 -0700

    writing tests to check if the list has city, delete city, and count cities

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
-
+    //The code below was taken from the instructions document for Lab 8
     private CustomList list;
     /**
      * create a mocklist for my citylist
@@ -34,6 +34,45 @@ public class CustomListTest {
         int listSize = list.getCount();
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(),listSize + 1);
+    }
+
+    // to complete the participation exercise I used the given lab code as a reference
+    /**
+     * Test checks if getting the count of cities works
+     * */
+    @Test
+    public void hasCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        City citynew = new City("Estevan", "SK");
+        list.addCity(citynew);
+        assertEquals(list.hasCity(citynew));
+    }
+    /**
+     * Test checks if getting the count of cities works
+     * */
+    @Test
+    public void countCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        list.addCity(new City("Calgary", "AB"));
+        list.addCity(new City("Toronto", "ON"));
+        assertEquals(3,list.getCount());
+    }
+
+    @Test
+    public void deleteCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        City citynew1 = new City("Estevan", "SK");
+        City citynew2 = new City("Calgary", "AB");
+        City citynew3 = new City("Toronto", "ON");
+        list.addCity(citynew1);
+        list.addCity(citynew2);
+        list.addCity(citynew3);
+        list.removeCity(citynew1);
+        assertEquals(2,list.getCount());
     }
 
 
